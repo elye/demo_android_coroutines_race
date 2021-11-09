a8aab5e38eebeff6addd1134dca17323384407c7
elye <elye.project@gmail.com>
2021-11-09 13:50:30 +1100

    Make it run on default on main thread

--- app/src/main/java/com/elyeproj/democoroutinesrace/MainActivity.kt
+++ app/src/main/java/com/elyeproj/democoroutinesrace/MainActivity.kt
@@ -5,10 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
 import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar
 import com.elyeproj.democoroutinesrace.databinding.ActivityMainBinding
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -38,34 +35,28 @@ class MainActivity : AppCompatActivity() {
     private fun startUpdate() {
         resetRun()
 
-        greenJob = GlobalScope.launch() {
+        greenJob = MainScope().launch() {
             startRunning(binding.progressBarGreen)
         }
 
-        redJob = GlobalScope.launch() {
+        redJob = MainScope().launch {
             startRunning(binding.progressBarRed)
         }
 
-        blueJob =GlobalScope.launch() {
+        blueJob =MainScope().launch() {
             startRunning(binding.progressBarBlue)
         }
     }
 
     private suspend fun startRunning(progressBar: RoundCornerProgressBar) {
-        runOnUiThread {
-            progressBar.progress = 0f
-        }
+        progressBar.progress = 0f
         while (progressBar.progress < 1000 && !raceEnd) {
-            runOnUiThread {
-                progressBar.progress += (1..10).random()
-            }
+            progressBar.progress += (1..10).random()
             delay(10)
         }
         if (!raceEnd) {
             raceEnd = true
-            runOnUiThread {
-                Toast.makeText(this, "${progressBar.tooltipText} won!", Toast.LENGTH_SHORT).show()
-            }
+            Toast.makeText(this, "${progressBar.tooltipText} won!", Toast.LENGTH_SHORT).show()
         }
     }
 
